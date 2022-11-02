3ccd76e06e66f8b45a18636d69e0d9187049c4cf
Yassine-Kh <yassine.khalsi98@gmail.com>
2022-11-02 09:53:28 +0100

    Commiting exos

---
+++ functions1.c
@@ -0,0 +1,502 @@
+//
+// Created by yassine on 12/10/22.
+//
+#include <unistd.h>
+#include <assert.h>
+#include <stdlib.h>
+
+void my_putchar(char c){
+    write(1, &c, 1);
+}
+
+void my_aff_alpha(){
+    char alphabet;
+
+    alphabet = 'a';
+    while (alphabet <= 'z'){
+        my_putchar(alphabet);
+        alphabet ++;
+    }
+}
+
+void my_aff_num(){
+    char i;
+
+    i = '0';
+    while (i <= '9'){
+        my_putchar(i);
+        i++;
+    }
+}
+
+void my_isneg(int n){
+    if (n >= 0) {
+        my_putchar('P');
+    } else {
+        my_putchar('N');
+    }
+}
+
+void aff_nbr(int cent, int deca, int unit) {
+    my_putchar(cent);
+    my_putchar(deca);
+    my_putchar(unit);
+    if (cent != '7' ) {
+        my_putchar(',');
+        my_putchar(' ');
+    }
+}
+
+void aff_comb() {
+    char cent;
+    char deca;
+    char unit;
+
+    cent = '1';
+    while (cent <= '7') {
+        deca = cent + 1;
+        while (deca <= '8') {
+            unit = '3';
+            while (unit <= '9') {
+                if (unit > deca && deca > cent) {
+                    aff_nbr(cent, deca, unit);
+                }
+                unit = unit + 1;
+            }
+            deca = deca + 1;
+        }
+        cent = cent + 1;
+    }
+}
+
+void my_putNbr(int param)
+{
+    if ( param < 0 )
+    {
+        my_putchar('-' );
+        if ( param == -2147483648 )
+        {
+            my_putchar( 2 + '0' );
+            param = 147483648;
+        }
+        else
+        {
+            param = param * -1;
+        }
+
+    }
+    if ( param < 9 )
+    {
+        my_putchar( param + '0' );
+    }
+    else
+    {
+        my_putNbr( param / 10 );
+        my_putchar(param%10 + '0' );
+    }
+}
+
+void my_putNbr2(int param)
+{
+    int unit;
+
+    if ( param < 0 )
+    {
+        my_putchar('-' );
+        if ( param == -2147483648 )
+        {
+            my_putchar( 2 + '0' );
+            param = 147483648;
+        }
+        else
+        {
+            param = param * -1;
+        }
+
+    }
+    unit = param;
+    while( unit > 9 )
+    {
+        unit = unit / 10;
+        if ( unit < 9 )
+        {
+            my_putchar( unit + '0' );
+            param = param / 10;
+            unit = param;
+        }
+    }
+}
+
+void my_swap(int* v1, int* v2)
+{
+    int v3;
+
+    v3 = *v2;
+    *v2 = *v1;
+    *v1 = v3;
+}
+
+int my_strlen(const char* s)
+{
+    int i;
+
+    i = 0;
+    while( s[i] != '\0' )
+    {
+        i++;
+    }
+    return(i);
+}
+
+void my_putstr(char* s)
+{
+    while( *s != '\0' )
+    {
+        my_putchar(*s);
+        s++;
+    }
+}
+
+char* evil_str(char s[])
+{
+    int len;
+    int i;
+    char temp;
+
+    i = 0;
+    len = my_strlen(s);
+    while(i <= len / 2 - 1)
+    {
+        temp = s[i];
+        s[i] = s[len - i - 1];
+        s[len - i - 1] = temp;
+        i++;
+    }
+    return(s);
+}
+
+int my_getnbr(char* s)
+{
+    int i;
+    int checkOneNumber;
+
+    checkOneNumber = 0;
+    i = 0;
+
+    while( *s != '\0' )
+    {
+        if ( *s >= '0' && *s <= '9' )
+        {
+            if (checkOneNumber == -1)
+            {
+                return 0;
+            }
+            checkOneNumber++;
+            i = i * 10;
+            i += *s -'0';
+        }
+        else
+        {
+            if ( checkOneNumber > 0 )
+            {
+                checkOneNumber = -1;
+            }
+        }
+        s++;
+    }
+    return i;
+}
+
+
+// calcule la puissance en iteratif
+int my_pow_it(int n, int power)
+{
+    int number;
+
+    assert( power >= 0 );
+    if ( power == 0 )
+    {
+        return 1;
+    }
+    number = n ;
+    while( power > 1 )
+    {
+        number = number * n;
+        power --;
+    }
+    return number;
+}
+
+// calcule la puissance en recursif
+int my_pow_rec(int n, int power)
+{
+    if ( power == 1 )
+    {
+        return n;
+    }
+    else if ( power == 0 )
+    {
+        return 1;
+    }
+    return n * my_pow_rec( n, power - 1);
+}
+
+// calcule la factorielle en iteratif
+int my_factoriel_it(int n)
+{
+    int range;
+
+    assert( n >= 0 );
+    if ( n == 0 )
+    {
+        return 1;
+    }
+    range = n - 1;
+    while ( range > 1 )
+    {
+        n = n * range ;
+        range --;
+    }
+    return n;
+}
+
+// calcule la factiorielle en recursif
+int my_factoriel_rec(int n)
+{
+    if ( n == 1 )
+    {
+        return n;
+    }
+    else if ( n == 0 )
+    {
+        return 1;
+    }
+    return n * my_factoriel_rec( n - 1 );
+}
+
+char *my_strcpy(char *dest, const char *src)
+{
+    while( *src != '\0' )
+    {
+        *dest = *src;
+        src += 1;
+        dest += 1;
+    }
+    *dest = '\0';
+    return dest;
+}
+
+char *my_strncpy(char *dest, char *src, size_t n)
+{
+    size_t len;
+
+    len = 0;
+    while( *(src + len) != '\0' && len < n)
+    {
+        *(dest + len) = *(src + len);
+        len ++;
+    }
+    while(len < n )
+    {
+        *(dest + len) = '\0';
+        len ++;
+    }
+    return dest;
+}
+
+int my_strcmp(char *s1, char *s2)
+{
+    int str1;
+    int str2;
+
+    str1 = 0;
+    str2 = 0;
+    while ( *s1 != '\0' )
+    {
+        if( *s2 != '\0' )
+        {
+            str2 += *s2;
+            s2 += 1;
+        }
+        str1 += *s1;
+        s1 += 1;
+    }
+    while ( *s2 != '\0' )
+    {
+        str2 += *s2;
+        s2 += 1;
+    }
+    return ( str1 - str2 );
+}
+
+int my_strncmp(char *s1, char *s2, size_t n)
+{
+    int str1;
+    int str2;
+    size_t len;
+
+    str1 = 0;
+    str2 = 0;
+    len = 0;
+    while ( *s1 != '\0' && len < n )
+    {
+        if( *s2 != '\0' && len < n )
+        {
+            str2 += *s2;
+            s2 += 1;
+        }
+        str1 += *s1;
+        s1 += 1;
+        len ++;
+    }
+    while ( *s2 != '\0' && len < n )
+    {
+        str2 += *s2;
+        s2 += 1;
+        len ++;
+    }
+    return ( str1 - str2 );
+}
+
+
+char *my_strstr(char *haystack, char *needle)
+{
+    int countNeedle;
+    int countHaystack;
+
+    countHaystack = 0;
+        while( *(haystack + countHaystack) != '\0')
+        {
+            countNeedle = 0;
+            while(*(haystack + countHaystack + countNeedle) == *(needle + countNeedle))
+            {
+                if (*(needle + countNeedle) == '\0')
+                {
+                    return haystack + countHaystack;
+                }
+                countNeedle ++;
+            }
+            if (*(needle + countNeedle) == '\0')
+            {
+                return haystack + countHaystack;
+            }
+            countHaystack ++;
+        }
+        return NULL;
+}
+
+char *my_str_upper(char *s)
+{
+    char *dest;
+    int pos;
+
+    pos = 0;
+    dest = malloc(sizeof(char) * (my_strlen(s)+1));
+    while (*(s + pos) != '\0')
+    {
+        if (*(s + pos) <= 'z' && *(s + pos) >= 'a')
+        {
+            *(dest + pos) = *(s + pos) - 'a' + 'A';
+        }
+        else
+        {
+            *(dest + pos) = *(s + pos);
+        }
+        pos++;
+    }
+    *(dest + pos) = '\0';
+    return dest;
+}
+
+char *my_str_lower(char *s)
+{
+    char *dest;
+    int pos;
+
+    pos = 0;
+    dest = malloc(sizeof(char) * (my_strlen(s)+1));
+    while (*(s + pos) != '\0')
+    {
+        if (*(s + pos) <= 'Z' && *(s + pos) >= 'A')
+        {
+            *(dest + pos) = *(s + pos) + 'a' - 'A';
+        }
+        else
+        {
+            *(dest + pos) = *(s + pos);
+        }
+        pos++;
+    }
+    *(dest + pos) = '\0';
+    return dest;
+}
+char *my_str_capitalize(char *s)
+{
+    char *dest;
+    int pos;
+
+    dest = malloc(sizeof(char) * (my_strlen(s)+1));
+    if (*s <= 'z' && *s >= 'a')
+    {
+        *dest = *s - 'a' + 'A';
+    }
+    else
+    {
+        *dest = *s;
+    }
+    pos = 1;
+    while (*(s + pos) != '\0')
+    {
+        if (*(s + pos) <= 'Z' && *(s + pos) >= 'A')
+        {
+            *(dest + pos) = *(s + pos) + 'a' - 'A';
+        }
+        else
+        {
+            *(dest + pos) = *(s + pos);
+        }
+        pos++;
+    }
+    *(dest + pos) = '\0';
+    return dest;
+}
+
+int my_str_is_alpha(char *s)
+{
+    if ((*s <= 'z' && *s >= 'a') || (*s <= 'Z' && *s >= 'A'))
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int my_str_is_num(char *s)
+{
+    if (*s <= '9' && *s >= '0')
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int my_str_is_printable(char *s)
+{
+    if (*s >= ' ' && *s <= '~')
+    {
+        return 1;
+    }
+    return 0;
+}
+
+char *my_strchr(char *s, char target)
+{
+    while (*s != '\0')
+    {
+        if (*s == target)
+        {
+            return s;
+        }
+        s ++;
+    }
+    return NULL;
+}
---
+++ main.c
@@ -0,0 +1,163 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "functions1.c"
+#include "test.c"
+
+void session1(){
+    printf("my_aff_alpha\n");
+    my_aff_alpha();
+    printf("\n\n");
+    printf("my_aff_num\n");
+    my_aff_num();
+    printf("\n\n");
+    printf("my_isneg(-5) \n");
+    my_isneg(-5);
+    printf("\n");
+    printf("my_isneg(5) \n");
+    my_isneg(5);
+    printf("\n");
+    printf("my_isneg(0) \n");
+    my_isneg(0);
+    printf("\n");
+    printf("my_isneg('okcden')\n");
+    my_isneg('5');
+    printf("\n\n");
+    printf("aff_comb\n");
+    aff_comb();
+}
+
+void session2()
+    {
+        printf("my_putNbr(0):\n");
+        my_putNbr(0);
+        printf("\n\n");
+        printf("my_putNbr(500):\n");
+        my_putNbr(500);
+        printf("\n\n");
+        printf("my_putNbr(-500):\n");
+        my_putNbr(-500);
+        printf("\n\n");
+        printf("my_putNbr(2 147 483 647):\n");
+        my_putNbr(2147483647);
+        printf("\n\n");
+        printf("my_putNbr(-2 147 483 648):\n");
+        my_putNbr(-2147483648);
+        printf("\n\n");
+    }
+
+void session3()
+{
+    int i;
+    int j;
+    int* pi;
+    int* pj;
+    char test[] = "whatever";
+
+    i = 1;
+    j = 2;
+    pi = &i;
+    pj = &j;
+    printf("values were equal to  %i %i\n", *pi, *pj);
+    printf("my_swap(*i, *j):\n");
+    my_swap(pi, pj);
+    printf("values are now %i %i\n\n", *pi, *pj);
+    printf("my_strlen( 'ma chaine de caracteres' )\n");
+    printf("%i\n\n", my_strlen("ma chaine de caracteres"));
+    printf("my_strlen( '' )\n");
+    printf("%i\n\n", my_strlen(""));
+    printf("my_strlen( 0 )\n");
+    printf("%i\n\n", my_strlen("\0"));
+
+    printf("my_putstr( 'ma chaine de caracteres' )\n");
+    my_putstr("ma chaine de caracteres");
+    printf("\n\n");
+
+    printf("evil_str( 'whatever' )\n");
+    evil_str(test);
+    printf(test);
+    printf("\n\n");
+
+    char * l = "fiebfi12345r";
+    printf("%i",my_getnbr(l));
+}
+
+void session4()
+{
+    int n;
+    int power;
+    char *dest;
+    char *src;
+
+    n = 5;
+    power = 2;
+    printf("my_pow_it(5, 2) = %i\n", my_pow_it(2,2));
+    printf("my_pow_rec(5, 2) = %i\n", my_pow_rec(2,2));
+    printf("my_factoriel_it(5) = %i\n", my_factoriel_it(5));
+    printf("my_factoriel_rec(5) = %i\n", my_factoriel_rec(2));
+
+    src = "test";
+    dest = malloc(sizeof(char) * my_strlen(src)*2);
+
+    my_strncpy(dest, src, 8);
+    printf('\0');
+
+    dest = "ttestijdi";
+
+    printf("La comparaison donne: %i \n",my_strncmp( dest, src, 8 ));
+
+    printf("Ca contient : %s\n", my_strstr( dest, src ));
+
+    printf("%s\n", my_str_upper(src));
+    char* test2 = "TESTINGthis";
+    printf("%s\n", my_str_lower(test2));
+    printf("%s\n", my_str_capitalize(test2));
+    char *test3 = ";";
+    printf("Is it alpha %i\n", my_str_is_alpha(test3));
+    char *test4 = "l";
+    printf("Is it num %i\n", my_str_is_num(test4));
+
+    printf("testing %i\n",  isprint(32));
+    printf("my_testing %i\n", my_str_is_printable("i"));
+    printf("searching target in %s", my_strchr("test", 't'));
+    //printf("%c", 116-'a'+'A');
+// a 97  z 122 A 65  Z 90
+}
+
+
+int main() {
+    //session1();
+    //test();
+    //session2();
+    //session3();
+    session4();
+    /*
+    printf("\n");
+    int k;
+    int u;
+    int * pu = &u;
+    *pu = 100;
+
+    k = *pu;
+    printf("Premier k est %i\n", k);
+    printf("Premier pu est %i\n", *pu);
+    *pu = 500;
+    printf("Second k est %i\n",k );
+    printf("Second pu est %i\n", *pu);
+    double x;
+    x=123.54547;
+    int c =x;
+    printf("%f\n", x/1);
+    printf("%i\n", c);
+    while(x > 1){
+        x --;
+    }
+    c = x;
+    printf("%i\n", c);
+    while (!( c <= x + 0.001 && c >= x - 0.001)) {
+        x= x * 10;
+        c=x;
+    }
+    printf("%i\n", c);
+     */
+    return 0;
+}
